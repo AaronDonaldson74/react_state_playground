fa6e3209f8b43830eb867edcb97b9b8a9023449e
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-26 11:38:05 -0600

    cleaning up the navigation container

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className="app">
-        <h1>DevCamp React Starter</h1>
-        <h2>React + React Router</h2>
+      <div className='container'>
+        <Router>
+          <div>
+            <NavigationContainer 
+            />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact-me" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
