b1339d81cc64a7dee023bf3fe1f4935182c9a769
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-27 12:58:57 -0600

    The counter buttons work!!

--- src/components/pages/counter.js
+++ src/components/pages/counter.js
@@ -6,23 +6,23 @@ class Counter extends Component {
         this.state = {number: 0};
 
         this.handleClickUp = this.handleClickUp.bind(this);
-        // this.handleClickDown = this.handleClickDown.bind(this);
+        this.handleClickDown = this.handleClickDown.bind(this);
     }
-
+    
     handleClickUp() {
-        this.setState(numchange => ({
-            number: state += 1
-        }));
+        this.setState(state => (this.state.number = this.state.number + 1));
     }
 
-    // handleClickDown()
+    handleClickDown() {
+        this.setState(state => (this.state.number = this.state.number - 1));
+    }
     render () {
         return (
             <div>
                 <h3>Counter</h3>
-                <div>{numchange()}</div>
-                <button className="btn" on Click={this.handleClickUp}>{this.state.number}, Up</button>
-                <button className="btn">Down</button>
+                <div>{this.state.number}</div>
+                <button className="btn" onClick={this.handleClickUp}>Up</button>
+                <button className="btn" onClick={this.handleClickDown}>Down</button>
             </div>
         );
     }
