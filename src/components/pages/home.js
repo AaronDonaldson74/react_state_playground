38ca8dadb7e37b0c4490ed7897c594eeb4a1b5a4
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-01 13:36:18 -0600

    refactored the homepage

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,8 +4,6 @@ export default function() {
     return (
         <div>
             <h1>Welcome to the world of State and Hooks</h1>
-            <h2>Currently make each componentn be classes</h2>
-            <h2>Message me when you finish the router so I can take a look</h2>
         </div>
     )
 }
